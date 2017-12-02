863caf27559b7907c8d00392e702fbb3d69809fc
Igor Sankovich <flexstudio.biz@gmail.com>
2017-12-02 23:13:33 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='miktapi',
+    version='0.1.0',
+    description='Mikrotik RouterOS API protocol',
+    url='https://github.com/mrin/miktapi',
+    author='Igor Sankovich',
+    author_email='flexstudio.biz@gmail.com',
+    license='MIT',
+    packages=['miktapi'],
+    zip_safe=False,
+    keywords='mikrotik routeros api',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
